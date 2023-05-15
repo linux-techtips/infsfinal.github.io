0cbc0094f79a1d35bd0989f3b0937624f6c30dc6
ThizGuy1016 <vavraacademy@gmail.com>
2023-05-15 10:54:17 -0500

    Added About for Amadeus

--- src/about.js
+++ src/about.js
@@ -22,6 +22,20 @@ a culture of security awareness, leveraging his leadership role to share knowled
 encourage peers to explore the rapidly evolving landscape of digital security. With a
 firm belief in collaborative learning, Orlando continues to drive efforts in defending
 against emerging cyber threats.
+
+<br>
+
+##About Amadeus
+
+![Amadeus](../static/amadeus.jpg)
+
+I am a computer engineering and cyber security with a minor in Asian studies student at
+UTRGV. I got my associates in 2018 from South Texas College, this program gave me the GPA
+to get a scholarship to pay for UTRGV, thus allowing me to complete my bachelor's. My passion
+is cyber security. However, this was not available to me until 2023 I have completed everything
+for my degree in computer engineering, and I hope to find an engaging career in cyber security
+in the near future.
+
 `;
 
   const sidebarFirstMarkdown = `
